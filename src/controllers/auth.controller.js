cd54b044e15ff7e55f36dd8a0e54a4126142245d
Rohit Bhatt <bhattrohed@gmail.com>
2025-08-07 10:43:31 +0530

    onboarding page logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -99,3 +99,46 @@ export async function logout(req,res){
     res.clearCookie("jwt");
     res.status(200).json({success:true, message:"Logout successful"});
 }
+
+export async function onboard(req, res){
+    try {
+        const userId = req.user._id;
+
+        const {fullName, bio, nativeLanguage, learningLanguage, location} = req.body;
+
+        if(!fullName || !bio || !nativeLanguage || !learningLanguage || !location){
+            return res.status(400).json({message:"All fields are required.", 
+                missingField:[
+                    !fullName && "fullName",
+                    !bio && "bio",
+                    !nativeLanguage && "nativeLanguage",
+                    !learningLanguage && "learningLanguage",
+                    !location && "location",
+                ].filter(Boolean),
+            })            
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            ...req.body, 
+            isOnboarded: true,
+        }, {new:true})
+
+        if(!updatedUser) return res.status(404).json({message:"User not found."})
+
+        try {
+            await upsertStreamUser({
+                id:updatedUser._id.toString(),
+                name:updatedUser.fullName,
+                image:updatedUser.profilePic || "",
+            });
+            console.log(`Stream user updated after onboarding for ${updatedUser.fullName}`);
+        } catch (streamError) {
+            console.log("Error updating stream user during onBoarding:",streamError.message)            
+        }
+
+        res.status(200).json({success:true, user: updatedUser});
+
+    } catch (error) {
+        console.log("Onboarding error:", error)
+    }
+}
