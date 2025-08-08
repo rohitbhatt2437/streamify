cb773993f607337e4604cdd16c9cfc26eec4f774
Rohit Bhatt <bhattrohed@gmail.com>
2025-08-08 20:51:10 +0530

    backend completed

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
+import chatRoutes from "./routes/chat.routes.js"
 import { connectDB } from "./lib/db.js";
 
 const app = express()
@@ -14,6 +15,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use('/api/chat', chatRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
